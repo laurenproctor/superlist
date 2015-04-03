4100467f97d996e7cbada94362362e8ee2d8294d
Lauren Proctor <laurenproctor32@gmail.com>
2015-04-02 23:56:51 -0400

    Created tasks and associated them with user

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150331234804) do
+ActiveRecord::Schema.define(version: 20150403035557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tasks", force: :cascade do |t|
+    t.text     "content"
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+    t.string   "state",      default: "to_do"
+    t.integer  "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
