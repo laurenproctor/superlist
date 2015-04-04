c0dc26f3455f6e479eaf2a14a03fd8761757a14e
Lauren Proctor <laurenproctor32@gmail.com>
2015-04-04 12:49:02 -0400

    Added state to tasks

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+# dependent: :destroy - if user deletes itself, all associated tasks will also be destroyed 
+  has_many :tasks, dependent: :destroy
 end
