067bbb2b608a06043d2235b3c17b426a98d4a2f0
Lauren Proctor <laurenproctor32@gmail.com>
2015-04-11 12:54:21 -0400

    Created test page with task panel styles

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   root :to => 'pages#home'
   get 'about' => 'pages#about'
+  get 'test' => 'pages#test'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
